dcc30fd2fc2023f0184c12d31c6cb80ec2e25674
David <davidchedrick@gmail.com>
2021-09-21 10:04:08 -0500

    commit tree 048c02bc5c0dcfa9dc1fb36cd998361911af4ef3

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
+  const re = /[\W_]/g;
+  const replaceSpace = word.toLowerCase().replace(re, '');
+  const reverseWord = replaceSpace.split('').reverse().join('')
+  console.log(word)
+  console.log(reverseWord)
+  return reverseWord === replaceSpace
+  
+ }
+ console.log(isPalindrome("Madam"))
 /* 
   Add your pseudocode here
+  first we have to reverse word.
+  split('') to split each letter into its own string
+  reverse() to reverse the letters
+  join('') to join the letters to a single string
+  return a booleen checking the reverse word with a === to the original word
+
+  function isPalindrome(word) {
+    const reverseWord = word.split('').reverse().join('')
+    return reverseWord === word
+  }
+  console.log(isPalindrome("racecar"))
+  //=>true
+  console.log(isPalindrome("live not on evil"))
+  //=>false 
+ Above code worked on a single word but not a sentence.
+
+  for muliple words we need to take out the spaces
+  const re = /[\W_]/g;  
+  const replaceSpace = word.replace(re, '');
+
+  console.log(isPalindrome("Madam"))
+  // => false
+Not working on Capitals, lets use a toLowerCase()
+
+ 
+
+
+
+
 */
 
 /*
@@ -20,6 +55,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("live not on evil"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("Madam"));
 }
 
 module.exports = isPalindrome;
